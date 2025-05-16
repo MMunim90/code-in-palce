e55e4b2887e505074a7d447cf73ad977abb0f543
MMunim <mmunim9.9.01@gmail.com>
2025-05-16 11:07:48 +0600

    fix game of nimm code

--- week4/The Game of Nimm.py
+++ week4/The Game of Nimm.py
@@ -4,23 +4,30 @@ def main():
     while TOTAL_STONES > 0:
         print(f"There are {TOTAL_STONES} stones left.")
         removed1 = int(input("Player 1 would you like to remove 1 or 2 stones? "))
-        if removed1 > 2:
+        while removed1 > 2:
             removed1 = int(input("Please enter 1 or 2: ")) 
         TOTAL_STONES = TOTAL_STONES - removed1
+        if(TOTAL_STONES == 0):
+            print("Player 2 wins!")
+            return
         print("")
         if TOTAL_STONES > 0:
             print(f"There are {TOTAL_STONES} stones left.")
             removed2 = int(input("Player 2 would you like to remove 1 or 2 stones? "))
-            if removed2 > 2:
+            while removed2 > 2:
                 removed2 = int(input("Please enter 1 or 2: ")) 
             TOTAL_STONES = TOTAL_STONES - removed2
+            if(TOTAL_STONES == 0):
+                print("Player 1 wins!")
+                return
             print("")
 
     if(removed1 > removed2):
         print("Player 1 wins!")
+    elif(removed1 == removed2):
+        print("Game over")
     else:
         print("Player 2 wins!")
-    #print("Game over")
 
 
 if __name__ == '__main__':
